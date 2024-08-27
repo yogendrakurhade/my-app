7b8786d3f564fcef0faba022f478ca477c638ca4
Yogendra Kurhade <yogendrakurhade@gmail.com>
2024-08-27 22:58:30 +0530

    In memory crud operations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AddEmployee } from "./components/AddEmployee";
+import { EditEmployee } from "./components/EditEmployee";
+import { GlobalProvider } from "./context/GlobalState";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddEmployee />} />
+          <Route path="/edit/:id" element={<EditEmployee />} />
+        </Routes>
+      </div>
+    </GlobalProvider>
   );
 }
-
-export default App;
